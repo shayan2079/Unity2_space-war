6fa35ba136d2c72f2f192955db8d45372d3f1853
shayan2079 <shahshayan2079@gmail.com>
2022-07-11 02:04:33 +0430

    Position for hitting objects modified

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,38 +5,46 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] int hitpoints = 1;
     [SerializeField] ParticleSystem HitVFX;
     [SerializeField] ParticleSystem DeathVFX;
+
     [SerializeField] Transform DeathVFXParent;
+
     [SerializeField] int deathScore = 10;
     [SerializeField] float explosionsSizeFactor = 1f;
+    [SerializeField] int hitpoints = 1;
+
+
+    List<ParticleCollisionEvent> collisionEvents;
 
     Scoring scoring;
 
     void Start()
     {
         scoring = FindObjectOfType<Scoring>();
+        collisionEvents = new List<ParticleCollisionEvent>();
     }
 
 
     void OnParticleCollision(GameObject other)
     {
+        other.GetComponent<ParticleSystem>().GetCollisionEvents(this.gameObject, collisionEvents);
 
-
-
-        if ((--hitpoints) > 0)
+        foreach (var collision in collisionEvents)
         {
-            ParticleSystem newParticle = Instantiate(HitVFX, transform);
-            newParticle.transform.localScale *= explosionsSizeFactor;
-        }
-        else if (hitpoints == 0)
-        {
-            ParticleSystem newParticle = Instantiate(DeathVFX, transform.position, Quaternion.identity);
-            newParticle.transform.localScale *= explosionsSizeFactor;
-            newParticle.transform.SetParent(DeathVFXParent);
-            scoring.UpdateScore(deathScore);
-            Destroy(this.gameObject);
+            if ((--hitpoints) > 0)
+            {
+                ParticleSystem newParticle = Instantiate(HitVFX, collision.intersection, Quaternion.identity);
+                newParticle.transform.SetParent(DeathVFXParent);
+            }
+            else if (hitpoints == 0)
+            {
+                ParticleSystem newParticle = Instantiate(DeathVFX, transform.position, Quaternion.identity);
+                newParticle.transform.localScale *= explosionsSizeFactor;
+                newParticle.transform.SetParent(DeathVFXParent);
+                scoring.UpdateScore(deathScore);
+                Destroy(this.gameObject);
+            }
         }
     }
 }
