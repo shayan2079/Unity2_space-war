b6372b1bbcd946c76a24aa862dba474ffcde204f
shayan2079 <shahshayan2079@gmail.com>
2022-07-11 00:06:38 +0430

    Scoring system added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int hitpoints = 1;
     [SerializeField] ParticleSystem HitVFX;
     [SerializeField] ParticleSystem DeathVFX;
     [SerializeField] Transform DeathVFXParent;
+    [SerializeField] int deathScore = 10;
     [SerializeField] float explosionsSizeFactor = 1f;
+
+    Scoring scoring;
+
+    void Start()
+    {
+        scoring = FindObjectOfType<Scoring>();
+    }
 
 
     void OnParticleCollision(GameObject other)
@@ -26,6 +35,7 @@ public class Enemy : MonoBehaviour
             ParticleSystem newParticle = Instantiate(DeathVFX, transform.position, Quaternion.identity);
             newParticle.transform.localScale *= explosionsSizeFactor;
             newParticle.transform.SetParent(DeathVFXParent);
+            scoring.UpdateScore(deathScore);
             Destroy(this.gameObject);
         }
     }
---
+++ Assets/Scripts/Scoring.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Scoring : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreBoard;
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreBoard.text = $"Score: {score}";
+    }
+}
